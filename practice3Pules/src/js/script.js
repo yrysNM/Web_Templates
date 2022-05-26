6b02e8c13460f376b390d918a43abaef32df9979
yrysNM <yrys.n3m@gmail.com>
2022-05-26 13:20:28 +0600

    templates pules done

--- practice3Pules/src/js/script.js
+++ practice3Pules/src/js/script.js
@@ -59,6 +59,75 @@ $(document).ready(function(){
         $(".overlay,  #order").fadeIn("slow");
       });
     });
+
+    
+    
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required", 
+          email: {
+            required: true,
+            email: true,
+          }
+        }, 
+        messages: {
+          name: "Пожалуйста, введите свое имя",
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправилньно введен адрес почты"
+          }
+        }
+      });
+    }
+    
+    
+    validateForms("#consultation form"); 
+    validateForms("#consultation-form");
+    validateForms("#order form");
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $("form").submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $("#consultation, #order").fadeOut();
+        $(".overlay, #thanks").fadeIn("slow");
+ 
+
+
+        $("form").trigger("reset");
+      });
+      return false;
+    });
+
+    
+    
+    //Smooth scroll and pageup
+
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1600) {
+        $(".pageup").fadeIn();
+      }else {
+        $(".pageup").fadeOut();
+      }
+    });
+
+
+    $("a[href='#up']").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+    new WOW().init();
 });
 
 
